c2f7d0a2929ec62755317bcce238292dc0cfa4a4
caapricorn <caapricorn@mail.ru>
2021-12-17 19:01:53 +0300

    Scripted auto-commit on change (2021-12-17 19:01:53) by gitwatch.sh

--- src/main/java/CacheActor.java
+++ src/main/java/CacheActor.java
@@ -1,4 +1,10 @@
 import akka.actor.AbstractActor;
 
+import java.util.HashMap;
+
 public class CacheActor extends AbstractActor {
+
+    private final HashMap<String, Integer> storage = new HashMap<>();
+
+    
 }
