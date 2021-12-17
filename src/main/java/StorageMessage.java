d0a0ecc30adfdc3393d0cfe52cb3f00f2efa50cc
caapricorn <caapricorn@mail.ru>
2021-12-17 18:45:28 +0300

    Scripted auto-commit on change (2021-12-17 18:45:28) by gitwatch.sh

--- src/main/java/StorageMessage.java
+++ src/main/java/StorageMessage.java
@@ -1,2 +1,11 @@
 public class StorageMessage {
+
+    private String url;
+    private Integer time;
+
+    public StorageMessage(String url, Integer time) {
+        this.url = url;
+        this.time = time;
+    }
+
 }
